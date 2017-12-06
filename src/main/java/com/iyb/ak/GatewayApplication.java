6c53078f0849c23cb8bdec4128806f408d674a09
admin801122 <434329122@qq.com>
2017-12-06 11:40:37 +0800

    集成配置中心

--- src/main/java/com/iyb/ak/GatewayApplication.java
+++ src/main/java/com/iyb/ak/GatewayApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -32,4 +34,14 @@ public class GatewayApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		log.info(">>>>>>>>>>>>>>> sc-sun-gateway 启动完成<<<<<<<<<<<<<");
 	}
+
+
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties(){
+		return new ZuulProperties();
+	}
+
+
+
 }
